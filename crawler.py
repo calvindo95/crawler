aa200212ca9d889ab6ae746001d13a35898a2c62
Calvin <calvindo95@gmail.com>
2021-03-22 13:04:22 -0500

    Refactor and added author class

--- crawler.py
+++ crawler.py
@@ -5,59 +5,60 @@ import shutil
 import config
 import argparse
 import bs4
+class Author():
+    def __init__(self,author):
+        self.author = author
+        self.authorDirectory = os.path.join("./user/", f"{author}")
 
-# Checks if OP account directory exists; creates account directory if directory does not exist
-def Check_opAccount_Directory(account_Directory):
-    if os.path.isdir(account_Directory): pass
-    else: os.mkdir(account_Directory)
+    def Get_Author(self):
+        return self.author
 
-# Crawls through subreddit poster's account
-def Crawl_opAccount(opAccount, my_Reddit, timePeriod):
-    try:
-        for submissionTitle in my_Reddit.redditor(f"{opAccount}").submissions.new():
-            Search_Sumbissions(opAccount, submissionTitle)
-        for submissionTitle in my_Reddit.redditor(f"{opAccount}").submissions.top(timePeriod):
-            Search_Sumbissions(opAccount, submissionTitle)
-    except Exception as e: print(e)
+    def Get_AuthorDirectory(self):
+        return self.authorDirectory
 
-# Searches for submissions from poster
-def Search_Sumbissions(opAccount, submissionTitle):
-    fileTypes = [".jpg", "redgifs"]
-    submissionURL = submissionTitle.url
-    fileName = submissionURL.rsplit("/", 1)[1]
-    filePath = os.path.join(account_Directory, f"{opAccount}-{fileName}")
-    for i in range(len(fileTypes)):
-        if fileTypes[i] in submissionURL:
-            DownloadFile(filePath, fileTypes[i], submissionURL, opAccount)
-        else: pass
+    # returns a list of URLS, list of file names, and lists of file paths
+    def Get_URLs(self):  # make this private??
+        submissionsURLs = []
+        sortType = [my_Reddit.redditor(f"{self.Get_Author()}").submissions.hot(), 
+                    my_Reddit.redditor(f"{self.Get_Author()}").submissions.new(),
+                    my_Reddit.redditor(f"{self.Get_Author()}").submissions.top()]
+        for i in range(len(sortType)):
+            for submissionTitle in sortType[i]:
+                try:
+                    if '.jpg' in submissionTitle.url:
+                        submissionsURLs.append(submissionTitle.url)
+                    elif 'redgif' in submissionTitle.url:
+                        submissionsURLs.append(submissionTitle.url)
+                    elif 'gifv' in submissionTitle.url: # returns url containing '.mp4'
+                        submissionsURLs.append(submissionTitle.url.rsplit('.',1)[0]+'.mp4')
+                except Exception as e:
+                    print(e)
+            return submissionsURLs
+    
+    def Get_FileNames(self):
+        submissionsURLs = self.Get_URLs()
+        fileNames = []
+        for i in range(len(submissionsURLs)):
+            if '.jpg' in submissionsURLs[i]:
+                fileName = submissionsURLs[i].rsplit("/", 1)[1]
+                fileNames.append(f"{self.Get_Author()}-{fileName}")
+            elif 'redgif' in submissionsURLs[i]:
+                fileName = submissionsURLs[i].rsplit("/", 1)[1]+'.mp4'
+                fileNames.append(f"{self.Get_Author()}-{fileName}")
+            elif '.mp4' in submissionsURLs[i]:
+                fileName = submissionsURLs[i].rsplit("/", 1)[1]
+                fileNames.append(f"{self.Get_Author()}-{fileName}")
+        return fileNames
 
-# downloads images and videos
-def DownloadFile(filePath, fileType, submissionURL, opAccount):
-    if os.path.exists(filePath):
-        print(f"{opAccount} {filePath} already exists")
-    elif os.path.exists(filePath+'.mp4'):
-        print(f"{opAccount} {filePath}.mp4 already exists")
-    elif fileType == ".jpg":
-        r = requests.get(submissionURL, stream=True)
-        if r.status_code == 200:
-            with open(filePath, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-            print(f"{opAccount} {filePath} downloaded successfully")
-        else:
-            print(f"{opAccount} {filePath} failed to download")
-    elif fileType == "redgifs":
-        res = requests.get(f'{submissionURL}')
-        res.raise_for_status()
-        exampleSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-        elems = exampleSoup.select('#root source')
-        redgifURL = elems[2].get('src')
-        r = requests.get(redgifURL, stream=True)
-        if r.status_code == 200:
-            with open(filePath+'.mp4', 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-            print(f"{opAccount} {filePath}.mp4 downloaded successfully")
-        else:
-            print(f"{opAccount} {filePath}.mp4 failed to download")
+    def Get_FilePaths(self):
+        fileNames = self.Get_FileNames()
+        filePaths = []
+        for i in range(len(fileNames)):
+            if '.jpg' in fileNames[i]:
+                filePaths.append(os.path.join(self.Get_AuthorDirectory(), fileNames[i]))
+            elif '.mp4' in fileNames[i]:
+                filePaths.append(os.path.join(self.Get_AuthorDirectory(), fileNames[i]))
+        return filePaths
 
 parser = argparse.ArgumentParser()
 parser.add_argument("sortType", nargs='?', help="Sort by 'hot', 'new', 'rising', 'controversial', 'top'; default='hot'", \
@@ -73,16 +74,58 @@ my_Reddit = praw.Reddit(client_id=config.client_id, \
 if __name__ == "__main__":
     subredditName = config.subredditURL.rsplit('/', 1)[1]
     print(f"searching for {args.sortType} authors in {subredditName}")
-    timePeriods = ["hour", "day", "week", "month", "year", "all"]
     redditDictofMethods = {'hot':my_Reddit.subreddit(subredditName).hot(limit=None),        # dictionary of reddit methods
-    'new':my_Reddit.subreddit(subredditName).new(limit=None), 
-    'rising':my_Reddit.subreddit(subredditName).rising(limit=None), 
-    'controversial':my_Reddit.subreddit(subredditName).controversial(limit=None),
-    'top':my_Reddit.subreddit(subredditName).top(limit=None)}
-    for i in range(len(timePeriods)):
-        for const_sortType in redditDictofMethods:
-            for submission in redditDictofMethods[const_sortType]:
-                opAccount = submission.author
-                account_Directory = os.path.join("./user/", f"{opAccount}")
-                Check_opAccount_Directory(account_Directory)
-                Crawl_opAccount(opAccount, my_Reddit, timePeriods[i])
+                            'new':my_Reddit.subreddit(subredditName).new(limit=None), 
+                            'rising':my_Reddit.subreddit(subredditName).rising(limit=None), 
+                            'controversial':my_Reddit.subreddit(subredditName).controversial(limit=None),
+                            'top':my_Reddit.subreddit(subredditName).top(limit=None)}
+    for submission in redditDictofMethods[const_sortType]:
+        author = Author(submission.author)
+        submissionUrls = author.Get_URLs()
+        submissionFileNames = author.Get_FileNames()
+        submissionFilePaths = author.Get_FilePaths()
+
+        # Checks of author directory exists
+        if os.path.exists(author.Get_AuthorDirectory()):
+            pass
+        else: os.mkdir(author.Get_AuthorDirectory())
+
+        for i in range(len(submissionUrls)):
+            if os.path.exists(submissionFilePaths[i]):
+                print(f"{author.Get_Author()} {submissionFilePaths[i]} already exists")
+            elif '.jpg' in submissionUrls[i]:
+                try:
+                    r = requests.get(submissionUrls[i], stream=True)
+                    if r.status_code == 200:
+                        with open(submissionFilePaths[i], 'wb') as f:
+                            shutil.copyfileobj(r.raw, f)
+                        print(f"{author.Get_Author()} {submissionFilePaths[i]} downloaded successfully")
+                    else:
+                        print(f"{author.Get_Author()} {submissionFilePaths[i]} failed to download")
+                except Exception:
+                    print(f"{author.Get_Author()} {submissionFilePaths[i]} failed to download")
+            elif '.mp4' in submissionUrls[i]:
+                if 'redgif' in submissionUrls[i]:
+                    res = requests.get(f'{submissionUrls[i]}')
+                    res.raise_for_status()
+                    exampleSoup = bs4.BeautifulSoup(res.text, 'html.parser')
+                    elems = exampleSoup.select('#root source')
+                    redgifURL = elems[2].get('src')
+                    r = requests.get(redgifURL, stream=True)
+                    if r.status_code == 200:
+                        with open(submissionFilePaths[i], 'wb') as f:
+                            shutil.copyfileobj(r.raw, f)
+                        print(f"{author.Get_Author()} {submissionFilePaths[i]} downloaded successfully")
+                    else:
+                        print(f"{author.Get_Author()} {submissionFilePaths[i]} failed to download")
+                else:
+                    try:
+                        r = requests.get(submissionUrls[i], stream=True)
+                        if r.status_code == 200:
+                            with open(submissionFilePaths[i], 'wb') as f:
+                                shutil.copyfileobj(r.raw, f)
+                            print(f"{author.Get_Author()} {submissionFilePaths[i]} downloaded successfully")
+                        else:
+                            print(f"{author.Get_Author()} {submissionFilePaths[i]} failed to download")
+                    except Exception:
+                        print(f"{author.Get_Author()} {submissionFilePaths[i]} failed to download")
